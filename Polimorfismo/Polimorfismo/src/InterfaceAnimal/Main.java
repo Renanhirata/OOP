548cb8f63b06b6dd1c4d476943695bb250143813
Renan Hiramatsu <renanhiramatsu1a@gmail.com>
2025-09-01 15:03:57 -0300

    Add files via upload

---
+++ Polimorfismo/Polimorfismo/src/InterfaceAnimal/Main.java
@@ -0,0 +1,13 @@
+package InterfaceAnimal;
+
+public class Main {
+
+    public static void main(String[]args){
+        Lobo lobo = new Lobo();
+
+        lobo.caminhar();
+        lobo.correr();
+        lobo.dormir();
+        lobo.emitirSom();
+    }
+}
---
+++ Polimorfismo/Polimorfismo/src/Polimorfismo/Gato.java
@@ -0,0 +1,11 @@
+package Polimorfismo;
+
+public class Gato extends Animal{
+    public Gato(String nome, String sexo, String raca) {
+        super(nome, sexo, raca);
+    }
+    @Override
+    public void emitirSom() {
+        System.out.println("Miaauuuuuu.");
+    }
+}
