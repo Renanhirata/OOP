3bc58bb6e6368b8cc1358f63f03d5a71ff1dd82f
Renan Hiramatsu <renanhiramatsu1a@gmail.com>
2025-09-01 11:03:41 -0300

    Add files via upload

---
+++ Veiculo Heranca/VeiculoHeranca/src/Veiculo/Automovel.java
@@ -0,0 +1,29 @@
+package Veiculo;
+
+public class Automovel extends Veiculo {
+
+    private String motor;
+    private int numeroPortas;
+
+    public Automovel(String marca, String modelo, String cor, int ano, String motor, int numeroPortas) {
+        super(marca, modelo, cor, ano);
+        this.motor = motor;
+        this.numeroPortas = numeroPortas;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public void setNumeroPortas(int numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+}
